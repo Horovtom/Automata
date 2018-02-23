fe374759ee0604e71744f35588a7bb5c61ea5631
Horovtom <horovtom@fel.cvut.cz>
2018-02-23 21:09:52 +0100

    Deleting cmake-build-debug directory

--- cmake-build-debug/test/lib/googletest-release-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lactosis/Documents/School/Automata/test/lib/googletest-release-1.8.0/googlemock
-# Build directory: /home/lactosis/Documents/School/Automata/cmake-build-debug/test/lib/googletest-release-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
